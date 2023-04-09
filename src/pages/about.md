c994799f1f013ac487382d878f02e17d947462fb
affanabiyyu <affanabito.dev@gmail.com>
2023-04-09 18:01:55 +0700

    first commit

---
+++ src/pages/about.md
@@ -0,0 +1,19 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+navigation: "about"
+---
+
+Hai.
+
+Aku suka nulis. Entah tentang apapun, entah bagus apa nggak, entah dibaca orang apa nggak.
+Dari SMA sudah sering nulis-nulis sendiri meski di Jurnal pribadi,
+Memang nggak bagus dan mungkin nggak guna,
+Tapi setidaknya di_proses_nya itu guna buat aku (?)
+
+Entah nanti jadinya website ini isinya gimana.
+Tapi tujuanku adalah pingin naruh tulisanku. Rencananya bakal aku sering-sering utak-atik juga, sembari belajar front-end lagi.
+
+Tulisannya bakal tentang macem-macem, sekedar pemikiran, mungkin tutorial, atau tentang game, olahraga, sampe psikologi, apapun setahuku, apapun yang aku suka. Wkwkwk.
+
+Okee makasii.
---
+++ tailwind.config.cjs
@@ -0,0 +1,85 @@
+/** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    // Remove the following screen breakpoint or add other breakpoints
+    // if one breakpoint is not enough for you
+    screens: {
+      sm: "640px",
+    },
+
+    // Uncomment the following extend
+    // if existing Tailwind color palette will be used
+
+    // extend: {
+    textColor: {
+      skin: {
+        base: withOpacity("--color-text-base"),
+        accent: withOpacity("--color-accent"),
+        inverted: withOpacity("--color-fill"),
+      },
+    },
+    backgroundColor: {
+      skin: {
+        fill: withOpacity("--color-fill"),
+        accent: withOpacity("--color-accent"),
+        inverted: withOpacity("--color-text-base"),
+        card: withOpacity("--color-card"),
+        "card-muted": withOpacity("--color-card-muted"),
+      },
+    },
+    outlineColor: {
+      skin: {
+        fill: withOpacity("--color-accent"),
+      },
+    },
+    borderColor: {
+      skin: {
+        line: withOpacity("--color-border"),
+        fill: withOpacity("--color-text-base"),
+        accent: withOpacity("--color-accent"),
+      },
+    },
+    fill: {
+      skin: {
+        base: withOpacity("--color-text-base"),
+        accent: withOpacity("--color-accent"),
+      },
+      transparent: "transparent",
+    },
+    fontFamily: {
+      'mono': ['IBM Plex Mono', 'monospace'],
+      'sans' : ['rajdhani', 'ui-sans-serif', 'system-ui', "Segoe UI", "Helvetica Neue", 'Arial', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    },
+    fontWeight: {
+      extralight: '300',
+      light: '400',
+      normal: '500',
+      medium: '600',
+      semibold: '700',
+      bold: '900',
+    },
+    fontSize: {
+      sm: '0.8rem',
+      md: '1rem',
+      base: '1.2rem',
+      lg: '1.4rem',
+      xl: '1.7rem',
+      '2xl': '2rem',
+      '3xl': '2.391rem',
+      '4xl': '2.702rem',
+      '5xl': '3.202rem',
+    },
+  },
+  plugins: [require("@tailwindcss/typography")], 
+};
